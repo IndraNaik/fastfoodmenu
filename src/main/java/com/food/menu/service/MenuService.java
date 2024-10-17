49fff8568fbeae8f5893a609dc9a78313c5f0223
IndraNaik <pathlavath.indra@gmail.com>
2024-10-17 19:33:32 +0530

    modified

--- src/main/java/com/food/menu/service/MenuService.java
+++ src/main/java/com/food/menu/service/MenuService.java
@@ -4,7 +4,6 @@ import com.food.menu.entity.MenuItem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 
 public interface MenuService {
     MenuItem addMenuItem(MenuItem menuItemRequest);
